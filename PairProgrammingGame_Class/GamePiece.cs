ce10867354211c931e4dbb6ef2e42910efd47776
JeffRichardson1150 <jeffrichardson573@gmail.com>
2020-07-09 02:24:16 -0400

    initial commit

---
+++ PairProgrammingGame_Class/GamePiece.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PairProgrammingGame_Class
+{
+    public class GamePiece
+    {
+        public string Name { get; set; }
+        public int Points { get; set; }
+        public int XCoord { get; set; }
+        public int YCoord { get; set; }
+        public bool WasMoved { get; set; }
+        public bool IsEliminated { get; set; }
+
+        public GamePiece() { }
+        public GamePiece(string name, int points, int xCoord, int yCoord, bool wasMoved, bool isEliminated)
+        {
+            Name = name;
+            Points = points;
+            XCoord = xCoord;
+            YCoord = yCoord;
+            WasMoved = wasMoved;
+            IsEliminated = isEliminated;
+        }
+    }
+}
---
+++ PairProgrammingGame_Console/PlayGame.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PairProgrammingGame_Console
+{
+    class PlayGame
+    {
+        static void Main(string[] args)
+        {
+            PlayGameUI ui = new PlayGameUI();
+            ui.Run();
+        }
+    }
+}
